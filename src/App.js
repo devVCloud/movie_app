73a819e9db57b9630133e40123dabdab3bec346a
deVCloud <wa.01.ch@gmail.com>
2020-03-17 16:42:04 +0900

    #1.0 Creating your first React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import Potato from './potato';
+import PropTypes from "prop-types";
 
+/*
+
+const foodIlike = [
+  {
+    id:1,
+    name : "kimchi",
+    image : "https://i.ytimg.com/vi/yP1Gk7IlhwU/maxresdefault.jpg",
+    rating : 4.7
+  },
+  {
+    id:2,
+    name : "icecream",
+    image : "https://sugarspunrun.com/wp-content/uploads/2018/07/Ice-Cream-Cone-Cupcakes-Recipe-1-of-1-6.jpg",
+    rating : 4.1
+  },
+  {
+    id:3,
+    name : "macaron",
+    image : "http://cafenoli605.hgodo.com/data/goods/14/08/29/1890/1409271403337m0.jpg",
+    rating : 4.3
+  }
+];
+
+//function Food(props) {
+  //console.log(props.fav)
+function Food({name, picture, rating}){ //object 내부를 얻는방법 
+  return <div>
+    I LOVE {name}
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name}/>
+    </div>
+}
+
+Food.propTypes ={
+  name : PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+}
+/ *
+function renderFood(dish){
+  return <Food name = {dish.name} picture={dish.image}></Food>
+}
+* /
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodIlike.map(dish =>  //dish 는 object이며 현재 처리상태이다
+        <Food 
+          key={dish.id} 
+          name={dish.name} 
+          picture={dish.image} 
+          rating={dish.rating} />
+        )}
     </div>
   );
+  / *
+ return (
+  <div>
+    {foodIlike.map(renderFood)}
+  </div>
+ );
+ * /
+}
+*/
+
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+  add = () => {
+    //this.setState({ count : this.state.count + 1 });
+    this.setState(current => ({count : current.count + 1}));  //이 방법이 state를 set 할때, react에서 외부 상태에 의존하지않는 가장 좋은방법
+  };
+  minus = () => {
+    this.setState(current => ({ count : current.count - 1 }));
+  };
+
+  render(){
+    return <div>
+        <h1>Number {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+  }
 }
 
 export default App;
+
+// 2 1 Reusable Components with JSX
+/*
+   Component 에서 Component로, children component로 정보 보내기 
+   props 를 사용. 
+*/
+
+/*
+  const friends = ["dal", "mark","lynn"]
+  friends.map(friend => {  console.log(friend);return friend+"♥"; })
+  ====>["dal♥", "mark♥", "lynn♥"]
+  friend로 지칭해도 되고 어떤 이름을 붙혀도 된다. 내부 item들에 접근하기 위함일뿐
+*/
+
+/*
+  state는 우리가 동적 데이터와 함께 작업할때 생성된다.
+*/
+
+/*
+  function component는 function 이고 뭔가를 return 하며 screen에 표시된다.
+  class component는 class 이며 react component로 부터 확장되고, screen에 표시된다.
+  class는 state 가지며, state는 object이다.
+  state는 직접 변경해서는 안된다. 렌더할 수 없다. this.state.count = 1 (x)
+  대신 setState function을 호출하면 언제 setState를 호출할지, view를 새로고침하길 원하는지 알고
+  render function이 refresh된다.
+  >>setState를 호출하면 새 state와 render function 이 호출된다.
+  * state는 object이므로, setState는 새로운 state를 받아야한다.
+  react 는 virtual Dom을 이용하기 때문에 변한 부분만 바꾸면서, 매우빠르게, 깜박거림도 없당
+*/
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//react는 app.js component를 element에 넣으려고 한다
+//페이지의 소스 코드 보기에서는 app.js 내용을 볼수없다 > react는 소스코드에 처음부터 html을 넣지 않고, 
+//html에서 html을 추가하거나 제거하는법을 알고있다.
+//virtual이고 존재하지 않기 때문에 react가 빠르다
+//react는 component와 동작한다.
+//react는 component를 사용해서 html처럼 작성하려는 경우에 필요
+//jsx : js와 html사이의 조합. javascript안의 html
+////ReactDOM.render(<Potato />, document.getElementById('potato')); >> react application이 하나의 component만을 rendering한다.
+// App.js 에서 다른 component를 import하여 사용. App.js 참조
+// ***react app은 한번에 하나!의 component만 rendering할 수 있다
